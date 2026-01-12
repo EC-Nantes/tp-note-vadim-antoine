31993e9704c938092e529195079ae878cfa94949
Antoine GACHET3 <agachet44@gmail.com>
2026-01-12 14:04:01 +0100

    Premier commit création du java

---
+++ Tpnotemedev/src/main/java/fr/centrale/nantes/infosi/tpnotemedev/Tpnotemedev.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.centrale.nantes.infosi.tpnotemedev;
+
+/**
+ *
+ * @author agach
+ */
+public class Tpnotemedev {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
